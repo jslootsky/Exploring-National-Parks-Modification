d8017f46d236d0fe79d3f891a9192d74bf27d3c7
Joshua Slootsky <joshuaslootsky@gmail.com>
2025-10-22 16:58:50 -0400

    called temple news in homepage

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -4,11 +4,12 @@
  * @module HomePage
  * @returns {JSX.Element} The rendered home page component.
  */
-import React from 'react'
-import Welcome from './HomePage/Components/Welcome'
-import Buttons from './HomePage/Components/Buttons'
+import React from 'react';
+import Welcome from './HomePage/Components/Welcome';
+import Buttons from './HomePage/Components/Buttons';
 import yosemite from './HomePage/Assets/yosemite.jpg';
-import './Style/homepage.css'
+import TempleNews from './HomePage/Components/TempleNews';
+import './Style/homepage.css';
 import HighlightGallery from './HomePage/Components/HighlightGallery';
 const HomePage = () => {
   return (
@@ -16,10 +17,11 @@ const HomePage = () => {
     <div className = "home-page main-component">
         {/* <h1>Test Hello</h1> */}
         <Welcome/>
+        <TempleNews/>
         <HighlightGallery/>
         <Buttons/>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
