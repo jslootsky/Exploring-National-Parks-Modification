f904c1eafd9fc7e10b8170c8984521ce70306cfb
Joshua Slootsky <joshuaslootsky@gmail.com>
2025-10-23 14:58:16 -0400

    finalizing changes

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -5,10 +5,9 @@
  * @returns {JSX.Element} The rendered home page component.
  */
 import React from 'react';
-import TempleTwitterTimeline from './HomePage/Components/TempleTwitterTimeline.jsx';
 import Welcome from './HomePage/Components/Welcome.jsx';
 import Buttons from './HomePage/Components/Buttons.jsx';
-import yosemite from './HomePage/Assets/yosemite.jpg';
+import TempleTwitterTimeline from './HomePage/Components/TempleTwitterTimeline.jsx';
 import TempleNews from './HomePage/Components/TempleNews.jsx';
 import './Style/homepage.css';
 import HighlightGallery from './HomePage/Components/HighlightGallery.jsx';
@@ -18,19 +17,7 @@ const HomePage = () => {
     <div className="home-page main-component">
       {/* <h1>Test Hello</h1> */}
       <Welcome />
-      <section aria-labelledby="temple-alerts">
-        <h2 id="temple-alerts">Temple Alerts</h2>
-        <TempleTwitterTimeline
-          screenName="TempleAlert"
-          height="200"
-          theme="light"
-          chrome="noheader nofooter"
-          className="twitter-wrap"
-        />
-      </section>
-      {/*
-      <TempleNews />
-      */}
+      <TempleNews /> 
       <HighlightGallery />
       <Buttons />
     </div>
